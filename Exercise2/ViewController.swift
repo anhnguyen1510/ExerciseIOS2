dd1f837336c0caeb6a60049b442fb077b84a551d
Anh Nguyen Cong <anh.nguyen13@ntq-solution.com.vn>
2021-10-09 00:18:17 +0700

    Initial Commit

---
+++ Exercise2/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Exercise2
+//
+//  Created by Anhnguyen on 09/10/2021.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
